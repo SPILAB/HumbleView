e2d7b20ee750de6e5d97f0b7337688f24771abf9
SPILAB.NET <spilab@gmail.com>
2018-06-03 19:37:22 +0200

    Cleaning code

--- humbleviewimage/src/main/java/spilab/net/humbleviewimage/model/HumbleBitmapId.kt
+++
@@ -1,4 +0,0 @@
-package spilab.net.humbleviewimage.model
-
-internal data class HumbleBitmapId(val url: String = "",
-                                   val size: ViewSize = ViewSize(0, 0))
--- humbleviewimage/src/main/java/spilab/net/humbleviewimage/model/ViewSize.kt
+++
@@ -1,6 +0,0 @@
-package spilab.net.humbleviewimage.model
-
-internal data class ViewSize(val width: Int = 0, val height: Int = 0) {
-
-    fun isValid(): Boolean = (width != 0 && height != 0)
-}
